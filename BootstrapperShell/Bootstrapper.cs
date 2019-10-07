2c7a2f24c4f2f316b030ccd307bc79ca2f92af1d
pkuml <pkuml@outlook.com>
2019-10-08 05:31:07 +0800

    添加项目文件。

---
+++ BootstrapperShell/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using BootstrapperShell.Views;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using Unity;
+
+namespace BootstrapperShell {
+    class Bootstrapper : UnityBootstrapper {
+        protected override DependencyObject CreateShell() {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell() {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
