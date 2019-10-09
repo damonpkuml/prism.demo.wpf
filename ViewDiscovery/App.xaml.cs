e3e8a3cb68860063e78c547e99b53d8939b67092
pkuml <pkuml@outlook.com>
2019-10-10 07:52:26 +0800

    04-ViewDiscovery

---
+++ ViewDiscovery/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Ioc;
+using Prism.Unity;
+using ViewDiscovery.Views;
+
+namespace ViewDiscovery{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication{
+        protected override void RegisterTypes(IContainerRegistry containerRegistry){
+            
+        }
+
+        protected override Window CreateShell(){
+            return Container.Resolve<MainWindow>();
+        }
+    }
+}
