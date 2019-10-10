5839cb85d129b74ee92b799d29fd400258b0d0a9
pkuml <pkuml@outlook.com>
2019-10-11 07:40:08 +0800

    07-Modules - Code 通过代码  通过项目引用 代码加载模块

---
+++ ModuleA2/ModuleA2Module.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ModuleA2.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace ModuleA2 {
+    public class ModuleA2Module : IModule {
+        public void RegisterTypes(IContainerRegistry containerRegistry) {
+        }
+
+        public void OnInitialized(IContainerProvider containerProvider) {
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("ContentRegion", typeof(ViewA));
+        }
+    }
+}
---
+++ Modules2/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using Modules2.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+
+namespace Modules2 {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+    
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        /// <summary>
+        /// 注意和 通过配置文件加载模块的区别
+        /// </summary>
+        /// <param name="moduleCatalog"></param>
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<ModuleA2.ModuleA2Module>();
+        }
+    }
+}
