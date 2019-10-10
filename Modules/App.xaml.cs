063f61ca5d53d17e8f311e6e8848614aaeb6cc2a
pkuml <pkuml@outlook.com>
2019-10-10 11:12:45 +0800

    通过配置文件加载模块

---
+++ Modules/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+using Modules.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+
+namespace Modules {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry) {
+
+        }
+
+        /// <summary>
+        /// 项目启动时的主窗口
+        /// </summary>
+        /// <returns></returns>
+        protected override Window CreateShell() {
+            return Container.Resolve<MainWindow>();
+        }
+
+        /// <summary>
+        /// ***********通过配置文件加载模块必须
+        /// </summary>
+        /// <returns></returns>
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return new ConfigurationModuleCatalog();
+        }
+    }
+}
