4f700c6361485fa9d5e01071e0cf2c785abcccd7
pkuml <pkuml@outlook.com>
2019-10-11 22:26:55 +0800

    11-UsingDelegateCommands  委托命令的基本使用

---
+++ UsingDelegateCommands/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace UsingDelegateCommands.ViewModels {
+    public class MainWindowViewModel : BindableBase {
+
+        private bool _isEnable;
+
+        public bool IsEnable {
+            get { return _isEnable; }
+            set {
+                SetProperty(ref _isEnable, value);
+                ExecuteDelegateCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private string _outputText;
+
+        public string OutputText {
+            get { return _outputText; }
+            set { SetProperty(ref _outputText, value); }
+        }
+
+
+
+
+        /// <summary>
+        /// 最简单的委托命令
+        /// </summary>
+        public DelegateCommand ExecuteDelegateCommand { get; private set; }
+
+        /// <summary>
+        /// 观察可用属性
+        /// </summary>
+        public DelegateCommand DelegateCommandObservesProperty { get; private set; }
+
+        /// <summary>
+        /// 观察可用委托(上面基础上的封装)
+        /// </summary>
+        public DelegateCommand DelegateCommandObservesCanExecute { get; private set; }
+
+        /// <summary>
+        ///通用带参数的委托命令(最好使用这种方式)
+        /// </summary>
+        public DelegateCommand<string> DelegateCommandWithParameter { get; private set; }
+
+        public MainWindowViewModel() {
+            ExecuteDelegateCommand = new DelegateCommand(Execute, CanExecute);
+            DelegateCommandObservesProperty = new DelegateCommand(Execute, CanExecute).ObservesProperty(() => IsEnable);
+            DelegateCommandObservesCanExecute=new DelegateCommand(Execute).ObservesCanExecute(()=>IsEnable);
+            DelegateCommandWithParameter=new DelegateCommand<string>(ExecuteWithParameter).ObservesCanExecute(()=>IsEnable);
+        }
+
+        private void ExecuteWithParameter(string obj)
+        {
+            OutputText = obj;
+        }
+
+        private void Execute() {
+            OutputText = $"Time:{DateTime.Now}";
+        }
+
+        private bool CanExecute() {
+            return _isEnable;
+        }
+    }
+}
