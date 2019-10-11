f5346177fbda8f00787a30319fc2558fb2d6f348
pkuml <pkuml@outlook.com>
2019-10-11 08:11:27 +0800

    07-Modules -从目录加载模块

---
+++ ModuleA3/ModuleA3Module.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ModuleA3.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace ModuleA3 {
+    public class ModuleA3Module : IModule {
+        public void RegisterTypes(IContainerRegistry containerRegistry) {
+        }
+
+        public void OnInitialized(IContainerProvider containerProvider) {
+            var regionManger = containerProvider.Resolve<IRegionManager>();
+            regionManger.RegisterViewWithRegion("ContentRegion", typeof(ViewA));
+        }
+    }
+}
---
+++ Modules3/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Modules3.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+
+namespace Modules3 {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry) {
+
+        }
+
+        protected override Window CreateShell() {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override IModuleCatalog CreateModuleCatalog() {
+            return new DirectoryModuleCatalog() { ModulePath = @".\Modules3" };
+        }
+    }
+}
