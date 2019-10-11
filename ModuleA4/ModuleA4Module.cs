6f03d95dab29c416093823ceeb8c291a404fae61
pkuml <pkuml@outlook.com>
2019-10-11 08:41:38 +0800

    07-Modules - LoadManual  使用IModuleManager添加模块，和前面通过代码添加模块类似，只是后者初始模式为按需

---
+++ ModuleA4/ModuleA4Module.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ModuleA4.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace ModuleA4 {
+    public class ModuleA4Module : IModule {
+        public void RegisterTypes(IContainerRegistry containerRegistry) {
+        }
+
+        public void OnInitialized(IContainerProvider containerProvider) {
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("ContentRegion", typeof(ViewA));
+        }
+    }
+}
---
+++ Modules4/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using ModuleA4;
+using Modules4.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+
+namespace Modules4 {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry) {
+        }
+
+        protected override Window CreateShell() {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog) {
+            var moduleType = typeof(ModuleA4Module);
+            moduleCatalog.AddModule(new ModuleInfo() {
+                ModuleName = moduleType.Name,
+                ModuleType = moduleType.AssemblyQualifiedName,
+                InitializationMode = InitializationMode.OnDemand
+            });
+        }
+    }
+}
