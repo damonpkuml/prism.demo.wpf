a610df2e19af7c1b345dd2e48320dc728f82a1e5
pkuml <pkuml@outlook.com>
2019-10-13 05:37:58 +0800

    18-NavigationCallback  导航完成时回调

---
+++ 18-NavigationCallback/BasicRegionNavigation/BasicRegionNavigation/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using BasicRegionNavigation.Views;
+using ModuleA;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+
+namespace BasicRegionNavigation {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry) {
+        }
+
+        protected override Window CreateShell() {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog) {
+            moduleCatalog.AddModule<ModuleAModule>();
+        }
+    }
+}
---
+++ 18-NavigationCallback/BasicRegionNavigation/BasicRegionNavigation/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace BasicRegionNavigation.ViewModels {
+    public class MainWindowViewModel : BindableBase {
+        private IRegionManager _regionManager;
+        public DelegateCommand<string> NavigateCommand { get; private set; }
+
+        public MainWindowViewModel(IRegionManager regionManager) {
+            _regionManager = regionManager;
+            NavigateCommand = new DelegateCommand<string>(Navigate);
+        }
+
+        private void Navigate(string obj) {
+            _regionManager.RequestNavigate("ContentRegion", obj, NavigateCommplete);
+        }
+
+        /// <summary>
+        /// 导航完成时触发
+        /// </summary>
+        /// <param name="obj"></param>
+        private void NavigateCommplete(NavigationResult obj) {
+            MessageBox.Show($"导航到{obj.Context.Uri}完成");
+        }
+    }
+}
