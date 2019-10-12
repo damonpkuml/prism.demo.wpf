799f8b4dc01ae190d333dcc0d0adbe97908fde4a
pkuml <pkuml@outlook.com>
2019-10-13 05:28:57 +0800

    17-BasicRegionNavigation 基本区域导航

---
+++ 17-BasicRegionNavigation/BasicRegionNavigation/BasicRegionNavigation/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace BasicRegionNavigation.ViewModels {
+    public class MainWindowViewModel : BindableBase {
+        private IRegionManager _regionManager;
+        public DelegateCommand<string> NavigateCommand { get; private set; }
+
+        public MainWindowViewModel(IRegionManager regionManager) {
+            _regionManager = regionManager;
+            NavigateCommand = new DelegateCommand<string>(Navigate);
+        }
+
+        private void Navigate(string obj) {
+            _regionManager.RequestNavigate("ContentRegion", obj);
+        }
+    }
+}
---
+++ 17-BasicRegionNavigation/BasicRegionNavigation/ModuleA/ModuleAModule.cs
@@ -0,0 +1,15 @@
+﻿using ModuleA.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+
+namespace ModuleA {
+    public class ModuleAModule : IModule {
+        public void RegisterTypes(IContainerRegistry containerRegistry) {
+            containerRegistry.RegisterForNavigation<ViewA>();
+            containerRegistry.RegisterForNavigation<ViewB>();
+        }
+
+        public void OnInitialized(IContainerProvider containerProvider) {
+        }
+    }
+}
