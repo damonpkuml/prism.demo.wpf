5287876116ce1a1115547f9c2554306bf0152382
pkuml <pkuml@outlook.com>
2019-10-14 04:24:39 +0800

    25-NotificationRequest  弹出模态窗口(过时)

---
+++ 25-NotificationRequest/UsingPopupWindowAction/UsingPopupWindowAction/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using Prism.Ioc;
+using Prism.Unity;
+using UsingPopupWindowAction.Views;
+
+namespace UsingPopupWindowAction {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry) {
+        }
+
+        protected override Window CreateShell() {
+            return Container.Resolve<MainWindow>();
+        }
+    }
+}
---
+++ 25-NotificationRequest/UsingPopupWindowAction/UsingPopupWindowAction/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Commands;
+using Prism.Interactivity.InteractionRequest;
+using Prism.Mvvm;
+
+namespace UsingPopupWindowAction.ViewModels {
+    public class MainWindowViewModel : BindableBase {
+        private string _title = "弹出模块窗口";
+
+        public string Title {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public InteractionRequest<INotification> NotificationRequest { get; private set; }
+        public DelegateCommand ShowPopupCommand { get; private set; }
+        public MainWindowViewModel() {
+            NotificationRequest = new InteractionRequest<INotification>();
+            ShowPopupCommand = new DelegateCommand(ShowPopup);
+        }
+
+        private void ShowPopup() {
+            NotificationRequest.Raise(new Notification() { Content = "通知消息", Title = "通知" }, r => Title = "通知完成");
+        }
+    }
+}
