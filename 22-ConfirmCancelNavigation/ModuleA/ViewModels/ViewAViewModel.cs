b13c5a7f49027f2ac74f32b2df1ada6c3c3351e5
pkuml <pkuml@outlook.com>
2019-10-14 01:16:27 +0800

    22-ConfirmCancelNavigation  确认和取消导航；

---
+++ 22-ConfirmCancelNavigation/ModuleA/ViewModels/ViewAViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Windows;
+
+namespace ModuleA.ViewModels {
+    public class ViewAViewModel : BindableBase, IConfirmNavigationRequest {
+
+        private int _pageCount;
+
+        public int PageCount {
+            get { return _pageCount; }
+            set { SetProperty(ref _pageCount, value); }
+        }
+
+        public ViewAViewModel() {
+
+        }
+
+
+        public void OnNavigatedTo(NavigationContext navigationContext) {
+            PageCount++;
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext) {
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext) {
+
+        }
+
+        public void ConfirmNavigationRequest(NavigationContext navigationContext, Action<bool> continuationCallback) {
+            var result = true;
+            if (MessageBox.Show("确定导航？", "提醒", MessageBoxButton.YesNo) == MessageBoxResult.No)
+                result = false;
+            continuationCallback(result);
+        }
+    }
+}
